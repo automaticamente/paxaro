2908673eed6c94e6a3a2e6250cad59d8179564ee
Berto Yáñez <berto@ber.to>
2016-02-21 23:09:56 +0100

    Video generation

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const path = require('path');
 const Feeder = require('./lib/feeder');
+const video = require('./lib/video');
 
-const satelliteFeed = new Feeder(new Date(), path.join(__dirname, 'images'), 12);
+const outputFolder = path.join(__dirname, 'images');
+
+const satelliteFeed = new Feeder(new Date('February 21, 2016 9:24:00'), outputFolder, 12);
 
 satelliteFeed.getSeries()
-    .then(images => {
-        console.log(images);
-    })
-    .catch(error => console.log(error));
-
-//ffmpeg -r 4 -i %d.jpg -c:v libx264 test.mp4
+    .then(() => video('images/%d.jpg', 'current.mp4'))
+    .then(output => console.log(output))
+    .catch(error => new Error(error));
